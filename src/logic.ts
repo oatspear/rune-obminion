98d347476487c1034508115a955749a6277eaa58
Oatspear <oatspear@gmail.com>
2024-10-01 20:44:35 +0100

    Update logic.ts

--- src/logic.ts
+++ src/logic.ts
@@ -8,16 +8,34 @@
 import type { PlayerId, RuneClient } from "rune-sdk"
 
 // -----------------------------------------------------------------------------
+// Constants
+// -----------------------------------------------------------------------------
+
+// const PLAYER_0_SPAWN_L = 1
+// const PLAYER_0_SPAWN_R = 7
+const PLAYER_0_BASE = 4
+// const PLAYER_1_SPAWN_L = 22
+// const PLAYER_1_SPAWN_R = 28
+const PLAYER_1_BASE = 25
+
+// -----------------------------------------------------------------------------
 // Exports
 // -----------------------------------------------------------------------------
 
-export type Cells = (PlayerId | null)[]
+export interface UnitState {
+  owner: number
+  movement: number
+  attackDice: number
+}
+
+export type TileState = UnitState | null
+
+export type BoardState = TileState[]
+
 export interface GameState {
-  cells: Cells
-  winCombo: number[] | null
+  board: BoardState
   lastMovePlayerId: PlayerId | null
   playerIds: PlayerId[]
-  freeCells?: boolean
 }
 
 type GameActions = {
@@ -32,20 +50,10 @@ declare global {
 // Logic
 // -----------------------------------------------------------------------------
 
-function findWinningCombo(cells: Cells) {
+function isWinningMove(playerIndex: number, tileIndex: number): boolean {
   return (
-    [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ].find((combo) =>
-      combo.every((i) => cells[i] && cells[i] === cells[combo[0]])
-    ) || null
+    (playerIndex === 0 && tileIndex === PLAYER_1_BASE) ||
+    (playerIndex === 1 && tileIndex === PLAYER_0_BASE)
   )
 }
 
@@ -53,42 +61,33 @@ Rune.initLogic({
   minPlayers: 2,
   maxPlayers: 2,
   setup: (allPlayerIds) => ({
-    cells: new Array(9).fill(null),
-    winCombo: null,
+    board: new Array(28).fill(null),
     lastMovePlayerId: null,
     playerIds: allPlayerIds,
   }),
   actions: {
-    claimCell: (cellIndex, { game, playerId, allPlayerIds }) => {
+    claimCell: (tileIndex, { game, playerId, allPlayerIds }) => {
       if (
-        game.cells[cellIndex] !== null ||
-        playerId === game.lastMovePlayerId
+        game.board[tileIndex] !== null ||
+        playerId === game.lastMovePlayerId ||
+        playerId == null
       ) {
         throw Rune.invalidAction()
       }
 
-      game.cells[cellIndex] = playerId
+      const owner = game.playerIds.indexOf(playerId)
+      const movement = 2
+      const attackDice = 2
+      game.board[tileIndex] = { owner, movement, attackDice }
       game.lastMovePlayerId = playerId
-      game.winCombo = findWinningCombo(game.cells)
 
-      if (game.winCombo) {
+      if (isWinningMove(owner, tileIndex)) {
         const [player1, player2] = allPlayerIds
 
         Rune.gameOver({
           players: {
-            [player1]: game.lastMovePlayerId === player1 ? "WON" : "LOST",
-            [player2]: game.lastMovePlayerId === player2 ? "WON" : "LOST",
-          },
-        })
-      }
-
-      game.freeCells = game.cells.findIndex((cell) => cell === null) !== -1
-
-      if (!game.freeCells) {
-        Rune.gameOver({
-          players: {
-            [game.playerIds[0]]: "LOST",
-            [game.playerIds[1]]: "LOST",
+            [player1]: playerId === player1 ? "WON" : "LOST",
+            [player2]: playerId === player2 ? "WON" : "LOST",
           },
         })
       }
